34a822c7c6501a9e66c3e0d13bc9fc9ce1a3b1c0
TimB44 <tblamires22@gmail.com>
2025-02-11 12:44:23 -0700

    Define envirment struct

--- src/typecheck.rs
+++ src/typecheck.rs
@@ -1,51 +1,126 @@
-use std::collections::HashMap;
+use std::{
+    collections::{
+        self,
+        hash_map::{self, Entry},
+        HashMap,
+    },
+    fmt::Display,
+    ops::Deref,
+};
 
-use miette::miette;
+use miette::{miette, LabeledSpan, MietteDiagnostic, Severity};
 
 use crate::{
-    ast::{auxiliary::Binding, types},
+    ast::types::{self, Type},
     utils::Span,
 };
 
-struct StructInfo<'a> {
-    fields: Box<[(&'a str, Type<'a>)]>,
-    return_type: Type<'a>,
-}
-
 pub struct Environment<'a> {
     src: &'a [u8],
-    structs: HashMap<&'a str, StructInfo<'a>>,
+    struct_ids: HashMap<&'a str, usize>,
+    // Indexed by the struct id
+    struct_info: Vec<StructInfo<'a>>,
+}
+
+pub struct StructInfo<'a> {
+    fields: HashMap<&'a str, Typed>,
+    id: usize,
+}
+
+impl<'a> StructInfo<'a> {
+    pub fn fields(&self) -> &HashMap<&'a str, Typed> {
+        &self.fields
+    }
 }
 
 impl<'a> Environment<'a> {
     pub fn new(src: &'a [u8]) -> Self {
-        Self {
-            src,
-            structs: HashMap::from([(
-                "rgba",
-                StructInfo {
-                    fields: [
-                        ("r", Type::Float),
-                        ("g", Type::Float),
-                        ("b", Type::Float),
-                        ("a", Type::Float),
-                    ]
-                    .to_vec()
-                    .into_boxed_slice(),
+        let struct_ids = HashMap::from([("rgba", 0)]);
+        let struct_fields = vec![StructInfo {
+            fields: HashMap::from([
+                ("r", Typed::Float),
+                ("g", Typed::Float),
+                ("b", Typed::Float),
+                ("a", Typed::Float),
+            ]),
+            id: 0,
+        }];
 
-                    return_type: Type::Struct("rgba"),
-                },
-            )]),
+        Self {
+            struct_ids,
+            struct_info: struct_fields,
+            src,
         }
     }
 
-    pub fn add_struct(
-        &self,
-        name: &Span,
-        params: &[Binding],
-        return_type: types::Type,
-    ) -> miette::Result<()> {
-        todo!()
+    pub fn add_struct(&mut self, name: Span, params: &[(Span, Type)]) -> miette::Result<()> {
+        let name_str = name.as_str(self.src);
+        match self.struct_ids.get(name_str) {
+            Some(_) => {
+                return Err(miette!(
+                    severity = Severity::Error,
+                    labels = vec![LabeledSpan::new(
+                        Some(format!("Struct {} already declared", name_str)),
+                        name.start(),
+                        name.len(),
+                    )],
+                    "Redefinition of struct"
+                ))
+            }
+            None => (),
+        }
+
+        let mut fields = HashMap::with_capacity(params.len());
+
+        for (field_name, field_type) in params {
+            let field_name_str = field_name.as_str(self.src);
+            let entry = match fields.entry(field_name_str) {
+                Entry::Occupied(_) => {
+                    let dup_loc = params
+                        .iter()
+                        .find(|(s, _)| s.as_str(self.src) == field_name_str)
+                        .unwrap()
+                        .0;
+
+                    return Err(miette!(
+                        severity = Severity::Error,
+                        labels = vec![
+                            LabeledSpan::new(
+                                Some("first delclared here".to_string()),
+                                dup_loc.start(),
+                                dup_loc.len(),
+                            ),
+                            LabeledSpan::new(
+                                Some("then here".to_string()),
+                                field_name.start(),
+                                field_name.len(),
+                            ),
+                        ],
+                        "Duplicate field: {} name for Struct: {}",
+                        field_name_str,
+                        name_str
+                    ));
+                }
+                Entry::Vacant(vacant_entry) => vacant_entry,
+            };
+
+            let converted_type = Typed::from_ast_type(field_type, self)?;
+            entry.insert(converted_type);
+        }
+
+        let id = self.struct_info.len();
+        self.struct_ids.insert(name_str, id);
+        self.struct_info.push(StructInfo { fields, id });
+
+        Ok(())
+    }
+
+    pub fn get_struct_id(&self, loc: Span) -> Option<usize> {
+        self.struct_ids.get(loc.as_str(self.src)).map(|x| *x)
+    }
+
+    pub fn get_struct_info(&self, id: usize) -> &StructInfo {
+        &self.struct_info[id]
     }
 
     pub fn src(&self) -> &[u8] {
@@ -53,45 +128,85 @@ impl<'a> Environment<'a> {
     }
 }
 
-pub trait Typecheck {
-    fn check(&self, env: &mut Environment) -> miette::Result<()>;
-}
-
 pub trait GetType {
-    fn get_type(&self, env: &Environment) -> miette::Result<Type>;
+    fn get_type(&self, env: &Environment) -> miette::Result<Typed>;
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Type<'a> {
+pub enum Typed {
     Int,
     Bool,
     Float,
+
     // Type of the array and the rank
-    Array(Box<Type<'a>>, u8),
-    Struct(&'a str),
+    Array(Box<Typed>, u8),
+
+    // Id of struct
+    Struct(usize),
     Void,
 }
 
-impl<'a> Type<'a> {
-    pub fn from_ast_type(t: &types::Type, src: &'a [u8]) -> Type<'a> {
-        match t.kind() {
-            types::TypeKind::Int => Type::Int,
-            types::TypeKind::Bool => Type::Bool,
-            types::TypeKind::Float => Type::Float,
-            types::TypeKind::Array(inner, rank) => {
-                Type::Array(Box::new(Self::from_ast_type(inner, src)), *rank)
+pub trait TypeState {}
+
+#[derive(Debug, Clone)]
+pub struct UnTyped {}
+impl TypeState for UnTyped {}
+impl TypeState for Typed {}
+
+impl Typed {
+    pub fn as_str(&self, env: &Environment) -> String {
+        match self {
+            Typed::Int => "int".to_string(),
+            Typed::Bool => "bool".to_string(),
+            Typed::Float => "float".to_string(),
+            Typed::Array(element_type, rank) => {
+                format!(
+                    "{}[{}]",
+                    element_type.as_str(env),
+                    ",".repeat((*rank as usize) - 1)
+                )
             }
-            types::TypeKind::Struct => Type::Struct(t.location().as_str(src)),
-            types::TypeKind::Void => Type::Void,
+            // TODO: not ideal
+            Typed::Struct(id) => env
+                .struct_ids
+                .iter()
+                .filter(|(_, v)| *v == id)
+                .next()
+                .unwrap()
+                .0
+                .to_string(),
+            Typed::Void => "void".to_string(),
         }
     }
 }
 
-fn expect_type<T: GetType>(node: &T, env: &Environment, expeted: Type) -> miette::Result<()> {
-    let actual = node.get_type(env)?;
-    if actual != expeted {
-        Err(miette!("Type mismatch"))
-    } else {
-        Ok(())
+impl Typed {
+    pub fn from_ast_type(t: &Type, env: &Environment) -> miette::Result<Self> {
+        Ok(match t.kind() {
+            types::TypeKind::Int => Typed::Int,
+            types::TypeKind::Bool => Typed::Bool,
+            types::TypeKind::Float => Typed::Float,
+            types::TypeKind::Array(inner, rank) => {
+                Typed::Array(Box::new(Self::from_ast_type(inner, env)?), *rank)
+            }
+            types::TypeKind::Struct => {
+                let id = env.get_struct_id(t.location()).ok_or_else(|| {
+                    let struct_name = t.location().as_str(env.src);
+                    miette!(
+                        severity = Severity::Error,
+                        labels = vec![LabeledSpan::new(
+                            Some(format!("could not find struct: {}", struct_name)),
+                            t.location().start(),
+                            t.location().len(),
+                        )],
+                        "Unkonwn struct: {}",
+                        struct_name,
+                    )
+                })?;
+
+                Typed::Struct(id)
+            }
+            types::TypeKind::Void => Typed::Void,
+        })
     }
 }
